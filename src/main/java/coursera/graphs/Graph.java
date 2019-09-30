0fce6ab84a60e75eced52b230664cf7f0762e5c0
Alex Lübeck <ndsvw@gmx.de>
2019-09-30 18:13:44 +0200

    added basic graph class

---
+++ src/main/java/coursera/graphs/Graph.java
@@ -0,0 +1,28 @@
+package coursera.graphs;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Graph {
+	private int nodes = 0;
+	private List<Set<Integer>> adjList = new ArrayList<Set<Integer>>();
+
+	public Graph(int nodes) {
+		this.nodes = nodes;
+		for (int i = 0; i < nodes; i++) {
+			adjList.add(new HashSet<Integer>());
+		}
+	}
+
+	public void addEdge(int u, int v) {
+		adjList.get(u).add(v);
+		adjList.get(v).add(u);
+	}
+
+	public Set<Integer> getAdjNodesOf(int u) {
+		return adjList.get(u);
+	}
+
+}
