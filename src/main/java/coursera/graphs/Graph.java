4d2e7dbb0c2ddb9cea7a1b421cd099166b9d0268
Alex Lübeck <ndsvw@gmx.de>
2019-10-01 09:01:49 +0200

    implemented dfs search

--- src/main/java/coursera/graphs/Graph.java
+++ src/main/java/coursera/graphs/Graph.java
@@ -29,4 +29,28 @@ public class Graph {
 		return adjList.get(u);
 	}
 
+	public Set<Integer> dfs(int u) {
+		boolean[] explored = new boolean[nodes];
+		for (int i = 0; i < explored.length; i++) {
+			explored[i] = false;
+		}
+		HashSet<Integer> reachedHashSet = new HashSet<Integer>();
+		dfs_rec(u, explored);
+		for (int i = 0; i < explored.length; i++) {
+			if (explored[i]) {
+				reachedHashSet.add(i);
+			}
+		}
+		return reachedHashSet;
+	}
+
+	private boolean[] dfs_rec(int u, boolean[] explored) {
+		explored[u] = true;
+		for (int v : adjList.get(u)) {
+			if (!explored[v]) {
+				dfs_rec(v, explored);
+			}
+		}
+		return explored;
+	}
 }
