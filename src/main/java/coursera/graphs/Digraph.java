6ac03050c9c24913e4aebc321742b787ca85341c
Alex Lübeck <ndsvw@gmx.de>
2019-10-02 17:12:21 +0200

    implemented SCC-algorithm (Tarjan's algorithm) for directed graphs

--- src/main/java/coursera/graphs/Digraph.java
+++ src/main/java/coursera/graphs/Digraph.java
@@ -1,6 +1,13 @@
 package coursera.graphs;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Digraph extends Graph {
+
+	private int scc_counter;
 
 	public Digraph(int nodes) {
 		super(nodes);
@@ -8,7 +15,11 @@ public class Digraph extends Graph {
 
 	public void addEdge(int u, int v) throws Exception {
 		if (u != v) {
-			adjList.get(u).add(v);
+			if (u < nodes && v < nodes) {
+				adjList.get(u).add(v);
+			} else {
+				throw new Exception("Trying to connect nodes that don't exist");
+			}
 		} else {
 			throw new Exception("An edge from a node to itself is not allowed");
 		}
@@ -27,4 +38,49 @@ public class Digraph extends Graph {
 		}
 		return revGraph;
 	}
+
+	public List<Set<Integer>> calculateSCC() {
+		// Tarjan's strongly connected components algorithm
+		List<Set<Integer>> sccList = new ArrayList<Set<Integer>>();
+		int[] order = calculateSCCOrder();
+
+		boolean[] explored = new boolean[nodes];
+		for (int i = 0; i < explored.length; i++) {
+			explored[i] = false;
+		}
+		for (int i = nodes - 1; i >= 0; i--) {
+			int u = order[i];
+			if (!explored[u]) {
+				sccList.add(dfs_rec(u, explored, new HashSet<Integer>()));
+			}
+		}
+		return sccList;
+	}
+
+	public int[] calculateSCCOrder() {
+		scc_counter = 0;
+		Digraph revDigraph = this.reverse();
+		boolean[] explored = new boolean[nodes];
+		int[] order = new int[nodes];
+		for (int i = 0; i < explored.length; i++) {
+			explored[i] = false;
+		}
+		for (int u = nodes - 1; u >= 0; u--) {
+			if (!explored[u]) {
+				revDigraph.modified_dfs_rec(u, explored, order);
+			}
+		}
+		return order;
+	}
+
+	private void modified_dfs_rec(int u, boolean[] explored, int[] order) {
+		explored[u] = true;
+		for (int v : adjList.get(u)) {
+			if (!explored[v]) {
+				modified_dfs_rec(v, explored, order);
+			}
+		}
+		order[scc_counter++] = u;
+	}
+
 }
